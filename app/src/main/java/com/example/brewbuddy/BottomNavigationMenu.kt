39001739074cb1b26d3acdb8792e0e4ab3d9ca74
cvnd <claudiavdioneda@gmail.com>
2023-06-16 15:33:35 -0400

    add color toggle

--- app/src/main/java/com/example/brewbuddy/BottomNavigationMenu.kt
+++ app/src/main/java/com/example/brewbuddy/BottomNavigationMenu.kt
@@ -10,9 +10,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.BottomAppBarDefaults
+import androidx.compose.material3.IconToggleButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -21,6 +24,8 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.brewbuddy.ui.theme.GreyLight
 import com.example.brewbuddy.ui.theme.SlateLight
+import androidx.compose.runtime.*
+import com.example.brewbuddy.ui.theme.GreyMedium
 
 sealed class BottomNavigationScreens(val route: String, @StringRes val resourceId: Int, @DrawableRes val icon: Int) {
     object Profile : BottomNavigationScreens("Profile", R.string.profile_route, R.drawable.icon_user)
@@ -40,11 +45,13 @@ public fun BottomNavigation(
         containerColor = MaterialTheme.colorScheme.background,
     ) {
         val currentRoute = currentRoute(navController)
+        var active by remember { mutableStateOf(BottomNavigationScreens.Profile.route) }
         items.forEach { item ->
             IconButton(
                 onClick = {
                     if (currentRoute != item.route) {
                         navController.navigate(item.route)
+                        active = item.route
                     }
                 },
                 modifier = Modifier.padding(start = 10.dp, end = 10.dp),
@@ -52,10 +59,11 @@ public fun BottomNavigation(
                 Icon(
                     painter = painterResource(id = item.icon),
                     contentDescription = "",
-                    tint = if (currentRoute == item.route) GreyLight else SlateLight,
+                    tint = if (active == item.route) GreyMedium else SlateLight,
                 )
 
             }
+
         }
     }
 }
