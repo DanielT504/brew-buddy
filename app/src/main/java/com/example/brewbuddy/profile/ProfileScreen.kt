8def54a9e127f28086d7cabf234aff791914a488
Mehr Sharma <mehrsharma@gmail.com>
2023-06-18 00:46:26 -0400

    adding card and temp user icon, todo: center icon, lower card, update username font

--- app/src/main/java/com/example/brewbuddy/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/brewbuddy/profile/ProfileScreen.kt
@@ -1,13 +1,45 @@
 package com.example.brewbuddy
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.brewbuddy.profile.CurrentUserViewModel
+import com.example.brewbuddy.ui.theme.InterFont
+
+
+@Composable
+fun CardWithIconAndTitle(modifier: Modifier) {
+    Card(modifier) {
+        val currentUserViewModel: CurrentUserViewModel = viewModel(viewModelStoreOwner = LocalNavGraphViewModelStoreOwner.current)
+        val user = currentUserViewModel.getUser()
+        val username = user!!.username
+        Row(modifier = Modifier.padding(16.dp)) {
+            Icon(Icons.Filled.Face, contentDescription = "Localized description")
+        }
+        Row(modifier = Modifier.padding(16.dp)) {
+            Text(
+                text = username,
+//                style = InterFont,
+            )
+        }
+    }
+}
+
 
 @Composable
 fun ProfileScreen(
@@ -15,8 +47,9 @@ fun ProfileScreen(
     val currentUserViewModel: CurrentUserViewModel = viewModel(viewModelStoreOwner = LocalNavGraphViewModelStoreOwner.current)
     val user = currentUserViewModel.getUser()
     val username = user!!.username
-    Surface(modifier = Modifier.fillMaxSize()) {
-        Text("Welcome, $username")
-    }
+//    Surface(modifier = Modifier.fillMaxSize()) {
+//        Text("Welcome, $username")
+//    }
 
+    CardWithIconAndTitle(modifier = Modifier.fillMaxSize())
 }
