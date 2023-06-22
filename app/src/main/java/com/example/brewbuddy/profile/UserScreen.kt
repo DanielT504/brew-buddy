d4dee5a01fa95df59feb017918c9e264af189c08
cvnd <claudiavdioneda@gmail.com>
2023-06-22 01:16:39 -0400

    add slideshow

--- app/src/main/java/com/example/brewbuddy/profile/UserScreen.kt
+++ app/src/main/java/com/example/brewbuddy/profile/UserScreen.kt
@@ -1,20 +1,34 @@
 package com.example.brewbuddy.profile
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
@@ -23,18 +37,80 @@ import com.example.brewbuddy.CardWithIconAndTitle
 import com.example.brewbuddy.ProfilePicture
 import com.example.brewbuddy.R
 import com.example.brewbuddy.getUser
+import com.example.brewbuddy.ui.theme.GreyLight
+import com.example.brewbuddy.ui.theme.GreyMedium
 import com.example.brewbuddy.ui.theme.TitleLarge
 
+private fun getIndex(currentIndex: Int, startIndex: Int, pageCount: Int): Int {
+    val diff = currentIndex - startIndex;
+    if (diff < 0) {
+        return currentIndex % pageCount
+    }
+    return diff % pageCount
+}
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun Carousel() {
+    val pageCount = 5
+    val bounds = 100 // arbitrarily large # to give the illusion of infinite scroll
+    val startIndex = bounds / 2
+
+    val pagerState = rememberPagerState(
+        initialPage = startIndex,
+        initialPageOffsetFraction = 0f,
+    ) {
+        bounds
+    }
+    val focusColor = GreyMedium
+    val unfocusedColor = GreyLight
+    Column(modifier=Modifier.fillMaxWidth()) {
+
+        HorizontalPager(
+            modifier = Modifier,
+            state = pagerState,
+            pageSpacing = 16.dp,
+            contentPadding = PaddingValues(horizontal = 100.dp),
+            pageSize = PageSize.Fixed(200.dp),
+            key = null,
+            beyondBoundsPageCount = 1,
+            pageContent = { index ->
+                val page = getIndex(index, startIndex, pageCount)
+
+                Box(contentAlignment = Alignment.Center) {
+                    CardWithIconAndTitle(modifier = Modifier.width(200.dp).height(150.dp))
+
+                }
+            },
+        )
+
+        Row(
+            modifier=Modifier.align(Alignment.CenterHorizontally).padding(top=10.dp),
+            horizontalArrangement = Arrangement.spacedBy(5.dp)
+        ) {
+            repeat(pageCount) { index ->
+                val color = if ( getIndex(pagerState.currentPage, startIndex, pageCount) == index) focusColor else unfocusedColor
+                Box(
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .size(12.dp)
+                        .background(color)
+                )
+            }
+
+        }
+
+    }
+}
 @Composable
 fun UserScreen(menuButton: @Composable () -> Unit) {
     val user = getUser()
 
-    Column() {
+    Column(modifier = Modifier.fillMaxSize()) {
         ProfileHeader(user, menuButton)
-        Surface() {
+        Column() {
             TitleLarge(text="Pinned Recipes")
+            Carousel()
         }
-        CardWithIconAndTitle(modifier = Modifier.fillMaxSize())
     }
 }
 
@@ -56,7 +132,7 @@ fun ProfileHeader(user: User, menuButton: @Composable () -> Unit) {
             Surface(
                 modifier= Modifier
                     .offset(y = profilePictureSize / 2)
-                    .height(profilePictureSize)
+                    .height(profilePictureSize - 25.dp)
                     .fillMaxWidth(),
                 shape= RoundedCornerShape(topEnd = 25.dp, topStart = 25.dp)
             ) {
@@ -76,7 +152,7 @@ fun ProfileHeader(user: User, menuButton: @Composable () -> Unit) {
 }
 @Composable
 fun ProfileBanner(@DrawableRes img: Int) {
-    Box(modifier = Modifier.height(220.dp)) {
+    Box(modifier = Modifier.height(200.dp)) {
         Image(
             painter = painterResource(id = img),
             contentDescription = "Profile Banner",
