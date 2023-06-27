7355b1f2f181e3ec66542068143bec598a81009b
Tanzim Pathan <tanzim@MacBook-Pro-3.local>
2023-06-27 19:52:11 -0400

    Add saved shops to the user profile, UI changes to store locator screen

--- app/src/main/java/com/example/brewbuddy/MainActivity.kt
+++ app/src/main/java/com/example/brewbuddy/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.brewbuddy
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.Observer
 import com.example.brewbuddy.profile.CurrentUserViewModel
 import com.example.brewbuddy.ui.theme.BrewBuddyTheme
@@ -14,6 +18,7 @@ class MainActivity : ComponentActivity() {
     // initializes the viewmodel for the currently logged in user and attaches it to the
     // scope of the main activity
     private val currentUserViewModel: CurrentUserViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -42,6 +47,5 @@ class MainActivity : ComponentActivity() {
             }
         })
     }
-
 }
 
--- app/src/main/java/com/example/brewbuddy/shoplocator/ShopLocatorScreen.kt
+++ app/src/main/java/com/example/brewbuddy/shoplocator/ShopLocatorScreen.kt
@@ -1,18 +1,23 @@
 package com.example.brewbuddy
 
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.material3.Text
 import androidx.compose.foundation.layout.padding
 
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
@@ -31,10 +36,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 import androidx.navigation.compose.*
+import com.example.brewbuddy.recipes.PreparationStep
+import com.example.brewbuddy.recipes.Recipe
+import com.example.brewbuddy.shoplocator.Store
+
 import com.example.brewbuddy.ui.theme.BrewBuddyTheme
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
@@ -47,37 +58,61 @@ import com.google.maps.android.compose.rememberCameraPositionState
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ShopLocatorScreen() {
-    Surface(modifier = Modifier.fillMaxSize(), color= MaterialTheme.colorScheme.background) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth().padding(25.dp)
-        ) {
-
+    Column (modifier = Modifier
+        .fillMaxSize()
+        .verticalScroll(rememberScrollState())){
+        Box(modifier = Modifier
+            .fillMaxWidth()) {
             MapWrapper()
-            StoreListSheet()
+            Box(modifier = Modifier
+                .fillMaxWidth()
+                .offset(y = 200.dp)) {
+                StoreListSheet()
+            }
         }
+
     }
 
 }
 @Composable
 @ExperimentalMaterial3Api
 fun StoreListSheet() {
-    Row(modifier = Modifier.fillMaxSize()) {
-        ModalDrawerSheet(modifier = Modifier
-            .height(400.dp)
-            .align(Alignment.Bottom)
-            .verticalScroll(
-                rememberScrollState()
-            ), drawerShape = MaterialTheme.shapes.extraSmall) {
-            Text("Shops nearby", modifier = Modifier.padding(16.dp))
+    Surface(
+        shape = RoundedCornerShape(topStart = 28.dp, topEnd = 28.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = Color.Transparent)
+    )
+    {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(900.dp),
+            contentAlignment = Alignment.TopCenter
+        ) {
+            Column(
+                modifier=Modifier.verticalScroll(rememberScrollState())
+            ) {
+                Text("Shops nearby", modifier = Modifier.padding(16.dp), style = MaterialTheme.typography.titleMedium, fontWeight = FontWeight.Bold)
             Divider()
-            StoreInfo(storeName = "C&D", address = "188 University Ave.", rating = 4.9, items = "Iced Oat Latte, Matcha Cappucino")
+                StoreInfo(
+                    storeName = "C&D",
+                    address = "188 University Ave.",
+                    rating = 4.9,
+                    items = "Iced Oat Latte, Matcha Cappucino"
+                )
             Divider()
-            StoreInfo(storeName = "Starbucks", address = "188 University Ave.", rating = 4.9, items = "Iced Oat Latte, Matcha Cappucino")
+                StoreInfo(
+                    storeName = "Starbucks",
+                    address = "188 University Ave.",
+                    rating = 4.9,
+                    items = "Iced Oat Latte, Matcha Cappucino"
+                )
             Divider()
             StoreInfo(storeName = "Café Waterloo", address = "188 University Ave.", rating = 4.9, items = "Iced Oat Latte, Matcha Cappucino")
             Divider()
             StoreInfo(storeName = "Café Waterloo", address = "188 University Ave.", rating = 4.9, items = "Iced Oat Latte, Matcha Cappucino")
+            }
         }
     }
 }
@@ -91,76 +126,110 @@ fun MapWrapper() {
         position = CameraPosition.fromLatLngZoom(waterloo, 15f)
     }
 
-    GoogleMap(
-        modifier = Modifier.height(300.dp),
-        cameraPositionState = cameraPositionState
-    ) {
-        Marker(
-            state = MarkerState(position = waterloo),
-            title = "C&D",
-            snippet = "Marker in Waterloo"
-        )
-        Marker(
-            state = MarkerState(position = starbs),
-            title = "Starbucks",
-            snippet = "Marker in Waterloo"
-        )
+
+        GoogleMap(
+            modifier = Modifier
+                .height(300.dp),
+            cameraPositionState = cameraPositionState
+        ) {
+            Marker(
+                state = MarkerState(position = waterloo),
+                title = "C&D",
+                snippet = "Marker in Waterloo"
+            )
+            Marker(
+                state = MarkerState(position = starbs),
+                title = "Starbucks",
+                snippet = "Marker in Waterloo"
+            )
+
     }
 }
 @Composable
 fun StoreInfo(storeName: String, address: String, rating: Double, items: String, modifier: Modifier = Modifier) {
-    Row(modifier = Modifier
-        .fillMaxWidth()
-) {
-        Column(modifier = Modifier
-            .padding(4.dp,16.dp,16.dp,16.dp)
+    Box(
+        modifier = Modifier
+            .fillMaxWidth(),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        Column(
+            horizontalAlignment = Alignment.Start,
         ) {
-            var selected by remember { mutableStateOf(false) }
-            IconButton(
-                onClick = {selected = !selected},
-            ) {
-                Icon(imageVector = if (!selected) Icons.Outlined.FavoriteBorder else Icons.Filled.Favorite,
-                    contentDescription = null)
-            }
-        }
-        Column() {
             Row(
                 modifier = Modifier
-                    .padding(0.dp,16.dp,16.dp,16.dp)
+                    .padding(vertical = 14.dp, horizontal = 4.dp)
                     .fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(
-                    text = "$storeName",
-                    color = MaterialTheme.colorScheme.primary,
-                    style = MaterialTheme.typography.bodyMedium
-                )
-                Text(
-                    text = "$address",
-                    color = MaterialTheme.colorScheme.primary,
-                    style = MaterialTheme.typography.bodySmall
-                )
-            }
-            Row(
-                modifier = Modifier
-                    .padding(0.dp,16.dp,16.dp,16.dp)
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(
-                    text = "$rating",
-                    color = MaterialTheme.colorScheme.primary,
-                    style = MaterialTheme.typography.bodySmall
-                )
-                Text(
-                    text = "$items",
-                    color = MaterialTheme.colorScheme.primary,
-                    style = MaterialTheme.typography.bodySmall
-                )
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            )
+            {
+                Row(horizontalArrangement = Arrangement.spacedBy(24.dp)) {
+                    Column(
+
+                    ) {
+                        var selected by remember { mutableStateOf(false) }
+                        IconButton(
+                            onClick = {
+                                store1.saved = !store1.saved
+                                selected = !selected
+                                      },
+                            modifier=Modifier.padding(0.dp, 24.dp, 0.dp, 16.dp)
+                        ) {
+                            Icon(
+                                imageVector = if (!selected) Icons.Outlined.FavoriteBorder else Icons.Filled.Favorite,
+                                contentDescription = null
+                            )
+                        }
+                    }
+                    Column() {
+                        Row(
+                            modifier = Modifier
+                                .padding(0.dp, 16.dp, 16.dp, 16.dp)
+                                .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Text(
+                                text = "$storeName",
+                                color = MaterialTheme.colorScheme.primary,
+                                style = MaterialTheme.typography.titleSmall
+                            )
+                            Text(
+                                text = "$address",
+                                color = MaterialTheme.colorScheme.primary,
+                                style = MaterialTheme.typography.titleSmall
+                            )
+                        }
+                        Row(
+                            modifier = Modifier
+                                .padding(0.dp, 16.dp, 16.dp, 16.dp)
+                                .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Text(
+                                text = "$rating",
+                                color = MaterialTheme.colorScheme.primary,
+                                style = MaterialTheme.typography.bodySmall
+                            )
+                            Text(
+                                text = "$items",
+                                color = MaterialTheme.colorScheme.primary,
+                                style = MaterialTheme.typography.bodySmall
+                            )
+                        }
+                    }
+                }
             }
         }
     }
 }
+
+public val store1 =
+    Store(storeName = "C&D",
+        address = "188 University Ave.",
+        latitude = 43.471701,
+        longitude = -80.543566,
+        items= listOf("Coffee", "Espresso"),
+        rating=4.9, saved = false)
 
 @Preview(showBackground = true)
 @Composable
