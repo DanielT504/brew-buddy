baa8b45a18bc7c4b603b6db08885ae76aa0aac09
cvnd <claudiavdioneda@gmail.com>
2023-07-13 21:51:51 -0400

    get started implementing integration with recipe screen

--- app/src/main/java/com/example/brewbuddy/recipes/Ingredient.kt
+++ app/src/main/java/com/example/brewbuddy/recipes/Ingredient.kt
@@ -8,3 +8,16 @@ data class Ingredient (
     val ingredientName: String,
     val ingredientComposite: IngredientComposition
 )
+
+//fun createIngredientsFromJSON(list: List<>) {
+//    return listOf(
+//        Ingredient(
+//            ingredientName = "Espresso",
+//            IngredientComposition(quantities = listOf("4.5 tbsp", "4oz"), subIngredientDetails = listOf("Finely-ground dark roast coffee", "Water"))
+//        ),
+//        Ingredient(
+//            ingredientName = "Foam Milk",
+//            IngredientComposition(quantities = listOf("4oz"), subIngredientDetails = listOf("Milk"))
+//        )
+//    )
+//}
--- app/src/main/java/com/example/brewbuddy/recipes/RecipesScreen.kt
+++ app/src/main/java/com/example/brewbuddy/recipes/RecipesScreen.kt
@@ -1,5 +1,6 @@
 package com.example.brewbuddy
 
+import android.util.Log
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
@@ -24,14 +25,6 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridItemSpan
-import com.example.brewbuddy.util.randomSampleImageUrl
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PageSize
@@ -44,19 +37,24 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import com.example.brewbuddy.ui.theme.GreyLight
-import com.example.brewbuddy.ui.theme.GreyMedium
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
 import coil.compose.AsyncImage
 import com.example.brewbuddy.recipes.Ingredient
 import com.example.brewbuddy.recipes.IngredientComposition
@@ -67,7 +65,13 @@ import com.example.brewbuddy.ui.theme.Brown
 import com.example.brewbuddy.ui.theme.Cream
 import com.example.brewbuddy.ui.theme.GreenDark
 import com.example.brewbuddy.ui.theme.GreenLight
-import com.example.brewbuddy.randomSizedPhotos as randomSizedPhotos
+import com.example.brewbuddy.ui.theme.GreyLight
+import com.example.brewbuddy.ui.theme.GreyMedium
+import com.example.brewbuddy.util.randomSampleImageUrl
+import com.google.firebase.functions.FirebaseFunctions
+import org.json.JSONArray
+import java.util.Objects
+
 
 @Composable
 fun RecipesScreen(
@@ -147,6 +151,7 @@ private fun RecipeGridLayout(navController: NavHostController) {
              ) {
              Heading(text = "Picked for you")
          }
+             getRecipes()
             items(recipes) { recipe ->
                 RecipeCard(
                     title = recipe.recipeName,
@@ -207,7 +212,7 @@ private fun PopularCard(photo: Any) {
 @Composable
 private fun RecipeCard(title: String, photo: Any, navController: NavHostController) {
     Card(
-        onClick = { navController.navigate(route = RecipeNavigationScreens.IndividualRecipe.route + title, )},
+        onClick = { navController.navigate(route = RecipeNavigationScreens.IndividualRecipe.route + title)},
         shape = RoundedCornerShape(12.dp),
         elevation = CardDefaults.cardElevation(
             defaultElevation = 15.dp
@@ -417,8 +422,18 @@ var testTags= listOf(
     TagType(iconTint = Color.White, tagColor = Brown, tagText = "Espresso Machine", img = R.drawable.icon_countertops),
     TagType(iconTint = Color.White, tagColor = GreenDark, tagText = "Cappuccino", img = R.drawable.icon_store)
 )
-
-
+private fun getRecipes(): ArrayList<HashMap<String, Object>> {
+    var result = ArrayList<HashMap<String, Object>>();
+    FirebaseFunctions
+        .getInstance()
+        .getHttpsCallable("getRecipesMetadata")
+        .call()
+        .addOnCompleteListener() {task ->
+            result = task.result?.data as ArrayList<HashMap<String, Object>>
+            Log.d("RECIPES", result.toString())
+        }
+    return result;
+}
 private val recipes = listOf(
     Recipe(
             "Cappuccino Almond Pistachio",
